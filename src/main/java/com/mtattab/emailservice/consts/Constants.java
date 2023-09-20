0151af4eed833ac21524cc75c4c706cc055283da
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-09-19 22:13:00 -0400

    added cors

--- src/main/java/com/mtattab/emailservice/consts/Constants.java
+++ src/main/java/com/mtattab/emailservice/consts/Constants.java
@@ -5,7 +5,7 @@ public interface Constants {
     public static final String JSON ="application/json";
     public static final String CLAIM_EMAIL ="UserEmail";
     public static final String CLAIM_FULL_NAME ="UserFullName";
-    public static final String[] CORS = {"http://localhost:4200", "https://budget-app.mtattab.com"};
+    public static final String[] CORS = {"http://localhost:4200", "https://budget-app.mtattab.com","*"};
 
     public static final String BUDGET_MAPPING = "/v1/api/budget";
 
