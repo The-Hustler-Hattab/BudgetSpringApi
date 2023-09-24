cab4d7925658442e206b24ca03929e2284700736
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-09-24 17:34:35 -0400

    added csrf protection

--- src/main/java/com/mtattab/emailservice/config/ProjectSecurityConfig.java
+++ src/main/java/com/mtattab/emailservice/config/ProjectSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.mtattab.emailservice.config;
 
+import com.mtattab.emailservice.security_filters.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,28 +29,23 @@ public class ProjectSecurityConfig  {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        CsrfTokenRequestAttributeHandler requestAttributeHandler= new CsrfTokenRequestAttributeHandler();
+        requestAttributeHandler.setCsrfRequestAttributeName("_csrf");
 
-
-        http
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeRequests()
                 .requestMatchers("/swagger-ui/**","/v3/api-docs/**").permitAll()
                 .requestMatchers("/", "about").permitAll()
                 .requestMatchers("/actuator/health").permitAll()
 
                 .anyRequest().authenticated()
-//
-////
+
 //                .anyRequest().permitAll()
                 .and()
-                .csrf().disable()
+                .csrf((csrf) -> csrf.csrfTokenRequestHandler(requestAttributeHandler)
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
 
-
-
-//        http.authorizeRequests()
-//
-//
-//
-//        .anyRequest().authenticated()
                 .oauth2ResourceServer(oauth2ResourceServerCustomizer ->
                         oauth2ResourceServerCustomizer.jwt(jwtCustomizer -> jwtCustomizer.jwtAuthenticationConverter(jwtAuthenticationConverter)))
 
